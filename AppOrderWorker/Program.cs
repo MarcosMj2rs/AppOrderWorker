71125d910f2204b64303214799b53fe971a841c7
MARCOS JESUS <tp.marcos.jesus@totvspartners.com.br>
2022-07-01 18:37:02 -0300

    Add project files.

---
+++ AppOrderWorker/Program.cs
@@ -0,0 +1,44 @@
+﻿using AppOrderWorker.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+	channel.QueueDeclare(queue: "orderQueue",
+						 durable: false,
+						 exclusive: false,
+						 autoDelete: false,
+						 arguments: null);
+
+	var consumer = new EventingBasicConsumer(channel);
+	consumer.Received += (model, ea) =>
+	{
+		try
+		{
+			var body = ea.Body.ToArray();
+			var message = Encoding.UTF8.GetString(body);
+			var order = JsonSerializer.Deserialize<Order>(message);
+
+			Console.WriteLine($"Order: {order?.OrderNumber}|{order?.ItemName}|{order?.Price:N2}", message);
+
+			channel.BasicNack(ea.DeliveryTag, false, true);
+		}
+		catch (Exception ex)
+		{
+			channel.BasicConsume(queue: "orderQueue",
+								 autoAck: false,
+								 consumer: consumer
+								);
+		}
+	};
+	channel.BasicConsume(queue: "orderQueue",
+						 autoAck: false,
+						 consumer: consumer);
+
+	Console.WriteLine(" Press [enter] to exit.");
+	Console.ReadLine();
+}
